ee0f67b039c590b634c2aaa516c54bf2997bf258
AK <kulimbayev@gmail.com>
2025-10-31 01:50:37 +0500

    add loader

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,23 +1,42 @@
-import {Component, Input} from '@angular/core';
-import {AsyncPipe, NgIf} from '@angular/common';
-
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
   standalone: true,
-  imports: [
-    NgIf,
-    AsyncPipe
-  ],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.scss'
 })
-export class LoadingSpinnerComponent {
+export class LoadingSpinnerComponent implements OnChanges, OnDestroy {
 
   @Input() isLoading = false;
+  isSlowLoading = false;
+  private slowTimer: any;
 
-  constructor() {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['isLoading']) {
+      if (this.isLoading) {
+        this.startTimer();
+      } else {
+        this.resetTimer();
+      }
+    }
   }
 
+  private startTimer() {
+    this.isSlowLoading = false;
+    this.slowTimer = setTimeout(() => {
+      this.isSlowLoading = true;
+    }, 10000);
+  }
 
+  private resetTimer() {
+    clearTimeout(this.slowTimer);
+    this.isSlowLoading = false;
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.slowTimer);
+  }
 }
