d666b60fbcae3f661d07bfe24cb2a6f8f0be4aa6
Azamat <a.aitkazin@gmail.com>
2025-11-03 19:14:19 +0500

    fix environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://www.sanaq.gov.kz/sanaqui',
-    apiUrl: 'http://localhost:5033',
+    apiUrl: 'https://www.sanaq.gov.kz/sanaqui',
+    //apiUrl: 'http://localhost:5033',
 };
